57609a40e2e6a6e700bf15f6064421ae766adfeb
Dylan Marley <dylanjmarley01@gmail.com>
2025-12-11 14:54:50 +0000

    feat: add ports to server

--- server/server.js
+++ server/server.js
@@ -13,3 +13,21 @@
 //TODO: a route to read data from the database
 
 //TODO: a route to create data in the database
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
