c736e92a3523d4adde0a5baddd00c9aa3554f247
Dylan Marley <dylanjmarley01@gmail.com>
2025-12-11 14:27:40 +0000

    chore: add template code

---
+++ server/server.js
@@ -0,0 +1,15 @@
+//TODO: setup a server
+
+//imports
+
+//configs
+
+//port
+
+//root route
+
+//==========
+
+//TODO: a route to read data from the database
+
+//TODO: a route to create data in the database
